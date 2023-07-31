17df20b4f3ed80b5511b269d4d2010e9ad9b9be9
Nnanna <omokennanna832@gmail.com>
2023-07-31 16:47:34 +0100

    Initial Commit

---
+++ qlearningwithrl.py
@@ -0,0 +1,11 @@
+import gymnasium as gym
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.optimizers import Adam
+from rl.agents.dqn import Agent
+
+from introdqn import explore_env
+
+env = gym.make("CartPole-v1", render_mode = "human")
+explore_env(env)
